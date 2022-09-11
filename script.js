07dc7e8e73d2b73110ebc8c92a68a83945cae35d
kchen1999 <kchen174175@gmail.com>
2022-09-11 11:54:29 +1000

    added functionality to allow players to add marks to a specific spot on the board

--- script.js
+++ script.js
@@ -2,23 +2,57 @@
 const gameBoard = (() => {
     const board = new Array(9);
     const getBoard = () => board; 
+    const updateBoard = (index, playerSign) => {
+        board[index] = playerSign;
+    };
     return {
-        getBoard
+        getBoard,
+        updateBoard
     };
 })();
+
+//factory
+const Player = (playerSign) => {
+    const getPlayerSign = () => playerSign; 
+    return {
+        getPlayerSign
+    }; 
+};  
 
 //module
 const displayController = (() => {
     const player1 = Player('X'); 
-    const player2 = Player('O'); 
+    const player2 = Player('O');
+    let playerTurn = 1; 
+    const board = document.querySelector('.game-board');
+    const checkLegalMove = (index) => {
+        if(gameBoard.getBoard()[index] == null) {
+            console.log("legal move");
+            return true; 
+        }
+        console.log("illegal move");
+        return false; 
+    };
+    const startGame = () => {
+        board.addEventListener('click', e => {
+            const clickedIndex = [...e.target.parentNode.children].indexOf(e.target); 
+            if(playerTurn == 1 && checkLegalMove(clickedIndex)) {
+                gameBoard.updateBoard(clickedIndex, 'X');
+                e.target.textContent = 'X'; 
+                playerTurn = 0; 
+            }
+            else if(playerTurn == 0 && checkLegalMove(clickedIndex)) {
+                gameBoard.updateBoard(clickedIndex, 'O');
+                e.target.textContent = '0'; 
+                playerTurn = 1; 
+            }
+        });
+    };
+    return {
+        startGame
+    };
 })();
 
+displayController.startGame(gameBoard);
 
-//factory
-const Player = (sign) => {
-    const getSign = () => sign; 
-    return {
-        getSign
-    }; 
-};  
 
