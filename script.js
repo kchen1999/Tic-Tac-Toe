506afbfed24c8e6d0f75a3f410281b96e45d190b
kchen1999 <kchen174175@gmail.com>
2022-09-13 23:21:48 +1000

    added check for three in a row and tie

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //module
 const gameBoard = (() => {
-    const board = new Array(9);
+    const board = new Array(9).fill(null);
     const getBoard = () => board; 
     const updateBoard = (index, playerSign) => {
         board[index] = playerSign;
@@ -23,28 +23,55 @@ const Player = (playerSign) => {
 const displayController = (() => {
     const player1 = Player('X'); 
     const player2 = Player('O');
-    let playerTurn = 1; 
+    let playerTurn = 'X'; 
     const board = document.querySelector('.game-board');
     const checkLegalMove = (index) => {
         if(gameBoard.getBoard()[index] == null) {
-            console.log("legal move");
             return true; 
         }
-        console.log("illegal move");
         return false; 
     };
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8], 
+        [0, 3, 6],
+        [1, 4, 7], 
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const isGameOver = (clickedIndex) =>
+        winningCombinations.filter(combination => combination.includes(clickedIndex)).some(
+            (possibleCombination) => possibleCombination.every((index) =>
+                gameBoard.getBoard()[index] === playerTurn
+            )
+        );
+    ;
+
+    const isDraw = () => 
+        !gameBoard.getBoard().includes(null);
+    ;
+
     const startGame = () => {
         board.addEventListener('click', e => {
             const clickedIndex = [...e.target.parentNode.children].indexOf(e.target); 
-            if(playerTurn == 1 && checkLegalMove(clickedIndex)) {
+            if(playerTurn == 'X' && checkLegalMove(clickedIndex)) {
                 gameBoard.updateBoard(clickedIndex, 'X');
                 e.target.textContent = 'X'; 
-                playerTurn = 0; 
+                if(isGameOver(clickedIndex) || isDraw()) {
+                    console.log("Game Over");
+                }
+                playerTurn = '0'; 
             }
-            else if(playerTurn == 0 && checkLegalMove(clickedIndex)) {
-                gameBoard.updateBoard(clickedIndex, 'O');
+            else if(playerTurn == '0' && checkLegalMove(clickedIndex)) {
+                gameBoard.updateBoard(clickedIndex, '0');
                 e.target.textContent = '0'; 
-                playerTurn = 1; 
+                if(isGameOver(clickedIndex) || isDraw()) {
+                    console.log("Game Over");
+                }
+                playerTurn = 'X'; 
             }
         });
     };
